4f14042c7ee3e68663c4cc5f67795d0e9cfc5f0f
Raul Lages <raul.lages@hotmail.com>
2020-03-12 21:09:30 -0300

    pokedex

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="container-fluid">
+    <PaginaInicial />
+  </div>
+</template>
+
+<script>
+import PaginaInicial from './components/pagina-inicial.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PaginaInicial
+  }
+}
+</script>
